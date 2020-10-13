9c750cbb23b8d3a307543ac46b71c826ca601702
snabelA00 <ja000411@gmail.com>
2020-10-13 11:37:56 +0200

    now the list can show corisponding fake floor numbers

--- src/com/company/LuckySkyScraper.java
+++ src/com/company/LuckySkyScraper.java
@@ -3,9 +3,16 @@ package com.company;
 public class LuckySkyScraper {
 
     static public void floorList(int totalRealFloors){
+
         //loop through real floor numbers
+        var fakeFloor = 1;
         for( var realFloor = 1; realFloor <= totalRealFloors; realFloor++){
-            System.out.println("real floor number: "  + realFloor);
+            System.out.println("real floor: "  + realFloor + " --- " + "fake floor: " + fakeFloor);
+
+            // add 1 to fakeFloor and continue to add 1 as long as fake floor ends with 4 or 13
+            do {
+                fakeFloor++;
+            } while(fakeFloor % 10 == 4 || fakeFloor % 100 == 13);
         }
     }
 }
