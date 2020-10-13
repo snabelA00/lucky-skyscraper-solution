c41aa3136c068a53e7b249c9befc641f5ed8db43
snabelA00 <ja000411@gmail.com>
2020-10-13 11:09:52 +0200

    looping throught real floors numbers

--- src/com/company/LuckySkyScraper.java
+++ src/com/company/LuckySkyScraper.java
@@ -1,4 +1,11 @@
 package com.company;
 
 public class LuckySkyScraper {
+
+    static public void floorList(int totalRealFloors){
+        //loop through real floor numbers
+        for( var realFloor = 1; realFloor <= totalRealFloors; realFloor++){
+            System.out.println("real floor number: "  + realFloor);
+        }
+    }
 }
